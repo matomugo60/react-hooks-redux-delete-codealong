ce987f1185d91b858869a8d2c3a5697fa79af00e
matomugo60 <matomugo60@gmail.com>
2023-03-21 15:19:30 +0300

    Complete

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import Todo from "./Todo";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import Todo from './Todo'
 
 function TodoContainer() {
-  const todos = useSelector((state) => state.todos.entities);
+  const todos = useSelector((state) => state.todos.entities)
 
-  const todoList = todos.map((todo, index) => <Todo key={index} text={todo} />);
-  return <ul>{todoList}</ul>;
+  const todoList = todos.map((todo) => <Todo key={todo.id} todo={todo} />)
+  return <ul>{todoList}</ul>
 }
 
-export default TodoContainer;
+export default TodoContainer
